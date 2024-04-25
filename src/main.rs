f9f5cff515ce6831f747764b2a257b6b73bc0b2d
OliverDahlWagner <71690615+OliverDahlWagner@users.noreply.github.com>
2024-04-26 00:34:19 +0200

    changed where things are

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use std::env; // envoirment
 use std::process;
 
-use minigrep::Config;
+// use minigrep::Config;   // if the file is named lib.rs   (libraty crate, I guess, maybe)
+// mod thing; // if the file is names not lib.rs   (auto into -> module)
+
+// if more declare shit here
+mod thing {
+    pub mod thing;
+    // pub mod subfolder {   // if there was a folder in the folder
+    //     pub mob subfile;
+    // }
+}
 
 fn main() {
-    let config = Config::new(env::args()).unwrap_or_else(|err| {
+    let config = thing::thing::Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem with parsing arguments: {}", err); // eprint is to print to the standard erro stream
         process::exit(0); // stop the process when aruguments are wrong, code 0 means successfull exit
     });
@@ -15,7 +24,7 @@ fn main() {
     println!("");
 
     // if run() returns an error do the block
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = thing::thing::run(config) {
         eprintln!("Application Error: {}", e);
         process::exit(0);
     }
